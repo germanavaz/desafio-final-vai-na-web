7e3c3e15a8952384f7cbaf8441a4800679ebaefb
germanavaz <gmsvaz@gmail.com>
2024-11-27 19:26:05 -0300

    page queroDoar

--- src/pages/QueroDoar/queroDoar.jsx
+++ src/pages/QueroDoar/queroDoar.jsx
@@ -1,9 +1,26 @@
 import S from './queroDoar.module.scss';
+import iconeLivro from '../../assets/IconeLivro.png'
 
 export default function QueroDoar() {
     return (
-      <section className={S.secao}>
-        <h2>EU SOU O COMPONENTE QUERO DOAR</h2>
-      </section>
+      <section className={S.queroDoar}>
+            <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
+            <section className={S.formulario}>
+                <div>
+                    <img src={iconeLivro} alt="" />
+                    <legend>
+                        Informações do Livro
+                    </legend>
+                </div>
+
+                <form action="">
+                    <input type="text" name="titulo" id="titulo" placeholder="Título" />
+                    <input type="text" name="categoria" id="categoria" placeholder="Categoria" />
+                    <input type="text" name="autor" id="autor" placeholder="Autor" />
+                    <input type="url" name="linkImagem" id="linkImagem" placeholder="Link da Imagem" />
+                    <input type="submit" value="Doar" />
+                </form>
+            </section>
+        </section>
     );
   }
