e8f886b183c03a2fdff65c77c9aff082bd3c7a49
germanavaz <gmsvaz@gmail.com>
2024-11-27 17:34:05 -0300

    page inicio banner

--- src/pages/Inicio/inicio.jsx
+++ src/pages/Inicio/inicio.jsx
@@ -2,8 +2,8 @@ import S from './inicio.module.scss';
 
 export default function Inicio() {
     return (
-      <section className={S.secao1}>
-        <h2>EU SOU O COMPONENTE INICIO</h2>
+      <section className={S.bannerPrincipal}>
+        <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
       </section>
 
     );
