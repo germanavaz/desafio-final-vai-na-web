5938fc9059fda8960d6a339d6bd6690c972164c1
germanavaz <gmsvaz@gmail.com>
2024-11-27 18:40:25 -0300

    page inicio porque section

--- src/pages/Inicio/inicio.jsx
+++ src/pages/Inicio/inicio.jsx
@@ -1,10 +1,40 @@
 import S from './inicio.module.scss';
+import Icon1 from '../../assets/div1.png';
+import Icon2 from '../../assets/div2.png';
+import Icon3 from '../../assets/div3.png';
+import Icon4 from '../../assets/div4.png';
 
 export default function Inicio() {
     return (
-      <section className={S.bannerPrincipal}>
-        <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
-      </section>
+      <main>
+        <section className={S.bannerPrincipal}>
+          <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+        </section>
+        
+        <article className={S.porque}>
+        <h2>Por que devo doar?</h2>
+          <section className={S.cards}>
+            <div>
+              <img src={Icon1} alt="" />
+              <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+            </div>
+            <div>
+              <img src={Icon2} alt="" />
+              <p> Estimula o hábito da leitura e o aprendizado contínuo.</p>
+            </div>
+            <div>
+              <img src={Icon3} alt="" />
+              <p> Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+            </div>
+            <div>
+              <img src={Icon4} alt="" />
+              <p> Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+            </div>
+          </section>
+        </article>
+        
+      </main>
+      
 
     );
   }
