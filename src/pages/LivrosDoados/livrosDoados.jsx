b27067cad1bd21af9459cd1be4e3d8e78887749a
germanavaz <gmsvaz@gmail.com>
2024-11-28 18:43:28 -0300

    page livrosDoados

--- src/pages/LivrosDoados/livrosDoados.jsx
+++ src/pages/LivrosDoados/livrosDoados.jsx
@@ -1,9 +1,18 @@
 import S from './livrosDoados.module.scss';
+import livro from '../../assets/livro1.png'
 
 export default function LivrosDoados() {
     return (
       <section className={S.secao1}>
-        <h2>EU SOU O COMPONENTE LIVROS DOADOS</h2>
+        <h2>Livros Doados</h2>
+        <div className={S.livro}>
+          <img src={livro} alt="" />
+          <div>
+          <p>O protagonista</p>
+          <p>Susanne Andrade</p>
+          <p>Ficção</p>
+          </div>
+        </div>
       </section>
     );
   }
